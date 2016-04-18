015c22772a5d581289801c9854094cac1fd31ab6
adelaide443 <adelaideborat@gmail.com>
2016-04-18 14:38:46 +0700

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get -y upgrade
+apt-get update
 apt-get install -y build-essential nano
 wget https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz
 tar xzf 3proxy-0.8.6.tar.gz
@@ -8,9 +8,9 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/benjamin74/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/adelaide443/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 cd /etc/init.d/
-wget --no-check-certificate https://raw.github.com/benjamin74/3proxy/master/3proxyinit
+wget --no-check-certificate https://raw.github.com/adelaide443/3proxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
